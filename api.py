1319522c4201d42db2feb34844f64a11459ed2a1
SachinNegi1997 <sachinnegi2619@gamil.com>
2026-09-08 10:39:41 +0530

    add

--- api.py
+++ api.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
+from sysutil import all_detail
 # fastapi is file and FastAPI is class
 # pip install fastapi
 # pip install "fastapi [satandard]"
-
+#  import all_detail function from sysutil file
 app=FastAPI(title="utilities")
 
 @app.get("/home")
 
 def home():
 
- return {"hello" : "this is return"}
+ return {"Hello" : "testing for api " }
+
+
+
+@app.get("/info")
+# creatin app when type "/info"  fuction call
+def info(): 
+
+   return all_detail()
+
 
 # To execute fastapi use  fastapi dev
 
